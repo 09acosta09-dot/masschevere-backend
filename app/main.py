7349f7b5234c8827892aefef5a695fccc7679e3e
Carlos Acosta <09acosta009@gmail.com>
2026-07-28 13:43:20 -0500

    Agregar dominio www a CORS

--- app/main.py
+++ app/main.py
@@ -10,10 +10,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://masschevere.com",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    	"https://masschevere.com",
+    	"https://www.masschevere.com",
+    	"http://127.0.0.1:5500",
+    	"http://localhost:5500"
+   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
