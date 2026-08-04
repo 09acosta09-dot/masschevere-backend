a9319bb6d1439a158bddbd8a0acd9cdb818d8a96
Carlos Acosta <09acosta009@gmail.com>
2026-08-04 11:03:08 -0500

    Agregar recepción de imágenes de comprobantes

--- app/routes/comprobantes.py
+++ app/routes/comprobantes.py
@@ -1,13 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, HTTPException
 
 router = APIRouter(
     prefix="/comprobantes",
     tags=["Comprobantes"]
 )
 
+
 @router.get("/test")
 def test():
     return {
         "ok": True,
         "mensaje": "Ruta de comprobantes funcionando"
+    }
+
+
+@router.post("/subir")
+async def subir_comprobante(
+    archivo: UploadFile = File(...)
+):
+
+    if not archivo.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=400,
+            detail="Solo se permiten imágenes."
+        )
+
+    return {
+        "ok": True,
+        "nombre": archivo.filename,
+        "tipo": archivo.content_type
     }
